a76707997d1a545b2061c4a4d9cddd9e539de2e1
ivanneira <ien1983@gmail.com>
2017-10-17 10:15:45 -0300

    con bower

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ivan
+ * Date: 17/10/2017
+ * Time: 09:53 AM
+ */
+?>
+
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Encuesta de Drogas</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="css/bootstrap-select.min.css">
+
+
+</head>
+<body>
+
+<div class="container-fluid">
+
+    <div class="row">
+        <div class="col-md">
+
+            <h4>Respuestas:</h4>
+
+        </div>
+    </div>
+
+    <div class="row">
+
+    </div>
+
+</div>
+
+
+
+</body>
+
+
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/bootstrap-select.min.js"></script>
+</html>
