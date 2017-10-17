4df5e14e0b9dc01748a6ae6a5a53d2e7d89f6c8f
ivanneira <ien1983@gmail.com>
2017-10-17 10:39:13 -0300

    con bowerrc

--- index.php
+++ index.php
@@ -16,10 +16,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Encuesta de Drogas</title>
 
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/bootstrap-grid.min.css">
-    <link rel="stylesheet" href="css/bootstrap-reboot.min.css">
-    <link rel="stylesheet" href="css/bootstrap-select.min.css">
+    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bower_components/bootstrap-select/dist/css/bootstrap-select.min.css">
 
 
 </head>
@@ -46,7 +44,7 @@
 </body>
 
 
-<script src="js/jquery-3.2.1.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/bootstrap-select.min.js"></script>
+<script src="bower_components/jquery/dist/jquery.min.js"></script>
+<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="bower_components/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
 </html>
