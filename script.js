3ed9036bce2dd774ae70be89c9e2fc2c2548085f
ivanneira <ien1983@gmail.com>
2017-10-21 17:09:18 -0300

    qwe

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 /**
  * Created by Ivan on 17/10/2017.
  */
+
+
+
 $(function(){
 
 
@@ -9,10 +12,45 @@ $(function(){
         size: 4
     });
 
+    $("#numeroCuestionario").focus();
+
+
+    $(document).keypress(function(data){
+
+    });
+
+    $('input[name=bebida]').change(function(){
+
+        if($(this).val() == 0){
+
+            $(".alcohol").hide();
+        }else{
+            $(".alcohol").show();
+        }
+    });
+
+    $('input[name=droga]').change(function(){
+
+        if($(this).val() == 0){
+
+            $(".drogas").hide();
+        }else{
+            $(".drogas").show();
+        }
+    });
+
+
 });
 
 
+
+
+
+
 function enviarDatos() {
+
+    console.dir($("#frm_data").serialize());
+    /*
     $.ajax({
         type: "POST",
         async: true,
@@ -35,5 +73,5 @@ function enviarDatos() {
             event.stopPropagation();
             alert("error al enviar solicitud, reintente");
         }
-    });
+    });*/
 }
